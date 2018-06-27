9fda2b6d8b94905776f110e7d67f9cb134aa86a7
Kasmenai <kasmenai@gmail.com>
2018-06-27 14:58:09 +0300

    add genDiff for yaml

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,8 +1,11 @@
 import fs from 'fs';
 import genDiff from '../src';
 
+const expected = fs.readFileSync('__tests__/__fixtures__/expected').toString();
 test('genDiff_json', () => {
-  const expected = fs.readFileSync('__tests__/__fixtures__/expected').toString();
-
   expect(genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json')).toBe(expected);
+});
+
+test('genDiff_yml', () => {
+  expect(genDiff('__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.yml')).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import fs from 'fs';
 import { has, union, find } from 'lodash';
+import path from 'path';
+import parse from './parsers';
 
 const render = (arr) => {
   const str = arr.map((obj) => {
@@ -41,9 +43,9 @@ const typeActions = [
 
 const getTypeAction = (key, obj1, obj2) => find(typeActions, ({ check }) => check(key, obj1, obj2));
 
-const parse = (obj1, obj2) => {
+const buildArr = (obj1, obj2) => {
   const allKeys = union(Object.keys(obj1), Object.keys(obj2));
-  const newObj = allKeys.reduce((acc, key) => {
+  const newArr = allKeys.reduce((acc, key) => {
     const { type } = getTypeAction(key, obj1, obj2);
     return [...acc, {
       key,
@@ -52,17 +54,17 @@ const parse = (obj1, obj2) => {
       type,
     }];
   }, []);
-  return newObj;
+  return newArr;
 };
 
 export default (obj1, obj2) => {
   const contentsObj1 = fs.readFileSync(obj1).toString();
   const contentsObj2 = fs.readFileSync(obj2).toString();
-  const data1 = JSON.parse(contentsObj1);
-  const data2 = JSON.parse(contentsObj2);
+  const data1 = parse(contentsObj1, path.extname(obj1));
+  const data2 = parse(contentsObj2, path.extname(obj2));
 
-  const newObj = parse(data1, data2);
-  // console.log(newObj);
-  // console.log(render(newObj));
-  return render(newObj);
+  const newArr = buildArr(data1, data2);
+  // console.log(newArr);
+  // console.log(render(newArr));
+  return render(newArr);
 };
