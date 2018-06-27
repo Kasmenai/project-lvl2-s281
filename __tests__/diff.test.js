d01ff268a0353596b8eff236fb7a6a0fb4a47c1d
Kasmenai <kasmenai@gmail.com>
2018-06-27 13:10:10 +0300

    fix diff.test expected move to file

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,14 +1,8 @@
+import fs from 'fs';
 import genDiff from '../src';
 
-test('genDiff', () => {
-  const expected = `{
-    host: hexlet.io
-  - timeout: 50
-  + timeout: 20
-  - proxy: 123.234.53.22
-  - follow: false
-  + verbose: true
-}`;
+test('genDiff_json', () => {
+  const expected = fs.readFileSync('__tests__/__fixtures__/expected').toString();
 
   expect(genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json')).toBe(expected);
 });
