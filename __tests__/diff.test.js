71c3ea47c632fd3a2154fe09faaaa2dfce9951ff
Kasmenai <kasmenai@gmail.com>
2018-06-28 21:38:35 +0300

    fix diff.test expected

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,20 +1,22 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-const expected = fs.readFileSync('__tests__/__fixtures__/expected').toString();
-const expected2 = fs.readFileSync('__tests__/__fixtures__/expected2').toString();
 test('genDiff_json', () => {
+  const expected = fs.readFileSync('__tests__/__fixtures__/expected').toString();
   expect(genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json')).toBe(expected);
 });
 
 test('genDiff_yml', () => {
+  const expected = fs.readFileSync('__tests__/__fixtures__/expected').toString();
   expect(genDiff('__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.yml')).toBe(expected);
 });
 
 test('genDiff_ini', () => {
+  const expected = fs.readFileSync('__tests__/__fixtures__/expected').toString();
   expect(genDiff('__tests__/__fixtures__/before.ini', '__tests__/__fixtures__/after.ini')).toBe(expected);
 });
 
 test('genDiff_ast', () => {
+  const expected2 = fs.readFileSync('__tests__/__fixtures__/expected2').toString();
   expect(genDiff('__tests__/__fixtures__/before2.json', '__tests__/__fixtures__/after2.json')).toBe(expected2);
 });
