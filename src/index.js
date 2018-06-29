9788472417b4702883346c774eb59caf8d306a93
Kasmenai <kasmenai@gmail.com>
2018-06-29 10:56:41 +0300

    refactor index.js parse render

--- src/index.js
+++ src/index.js
@@ -1,68 +1,94 @@
 import fs from 'fs';
-import { has, union, find } from 'lodash';
+import _ from 'lodash';
 import path from 'path';
 import parse from './parsers';
 
-const render = (arr) => {
+
+const stringify = (obj, currDepth) => {
+  if (!_.isObject(obj)) {
+    return obj;
+  }
+  const tab = '    '.repeat(currDepth);
+  const qq = _.keys(obj)
+    .map(key => `${tab}    ${key}: ${stringify(obj[key], currDepth + 1)}`).join('\n');
+  return `{\n${qq}\n${tab}}`;
+};
+
+const render = (arr, i = 0) => {
+  const currDepth = i;
+  const tab = '    '.repeat(currDepth);
   const str = arr.map((obj) => {
     const {
       key, oldVal, newVal, type, children,
     } = obj;
+    const oldStr = `${key}: ${stringify(oldVal, currDepth + 1)}`;
+    const newStr = `${key}: ${stringify(newVal, currDepth + 1)}`;
     switch (type) {
       case 'deleted':
-        return `  - ${key}: ${oldVal}`;
+        return `${tab}  - ${oldStr}`;
       case 'added':
-        return `  + ${key}: ${newVal}`;
+        return `${tab}  + ${newStr}`;
       case 'changed':
-        return (children) ? `    ${key}: ${render(children)}`
-          : `  - ${key}: ${oldVal}\n  + ${key}: ${newVal}`;
+        return `${tab}  - ${oldStr}\n${tab}  + ${newStr}`;
+      case 'hasChildren':
+        return `${tab}    ${key}: ${render(children, currDepth + 1)}`;
       default:
-        return `    ${key}: ${oldVal}`;
+        return `${tab}    ${oldStr}`;
     }
   }).join('\n');
-  return `{\n${str}\n}`;
+  return `{\n${str}\n${tab}}`;
 };
 
 const typeActions = [
   {
     type: 'added',
-    check: (key, obj1) => !has(obj1, key),
+    check: (key, obj1) => !_.has(obj1, key),
+    oldValFn: () => null,
+    newValFn: (key, obj1, obj2) => obj2[key],
+    process: () => null,
   },
   {
     type: 'deleted',
-    check: (key, obj1, obj2) => !has(obj2, key),
+    check: (key, obj1, obj2) => !_.has(obj2, key),
+    oldValFn: (key, obj1) => obj1[key],
+    newValFn: () => null,
+    process: () => null,
   },
   {
     type: 'unchanged',
     check: (key, obj1, obj2) => obj1[key] === obj2[key],
+    oldValFn: (key, obj1) => obj1[key],
+    newValFn: (key, obj1, obj2) => obj2[key],
+    process: () => null,
+  },
+  {
+    type: 'hasChildren',
+    check: (key, obj1, obj2) => _.isObject(obj1[key]) && _.isObject(obj2[key]),
+    process: (obj1, obj2, f) => f(obj1, obj2),
+    oldValFn: () => null,
+    newValFn: () => null,
   },
   {
     type: 'changed',
     check: (key, obj1, obj2) => obj1[key] !== obj2[key],
+    oldValFn: (key, obj1) => obj1[key],
+    newValFn: (key, obj1, obj2) => obj2[key],
+    process: () => null,
   },
 ];
 
-const getTypeAction = (key, obj1, obj2) => find(typeActions, ({ check }) => check(key, obj1, obj2));
+const getTypeAction = (key, obj1, obj2) => _.find(typeActions, ({ check }) => check(key, obj1, obj2));
 
 const buildAST = (obj1, obj2) => {
-  const allKeys = union(Object.keys(obj1), Object.keys(obj2));
+  const allKeys = _.union(_.keys(obj1), _.keys(obj2));
   const newArr = allKeys.reduce((acc, key) => {
-    const { type } = getTypeAction(key, obj1, obj2);
-    if (type === 'changed' && (obj1[key] instanceof Object) && (obj2[key] instanceof Object)) {
-      return [...acc, {
-        key,
-        oldVal: null,
-        newVal: null,
-        type,
-        children: buildAST(obj1[key], obj2[key]),
-      }];
-    }
+    const { type, process, oldValFn, newValFn } = getTypeAction(key, obj1, obj2);
     return [...acc, {
       key,
-      oldVal: obj1[key],
-      newVal: obj2[key],
+      oldVal: oldValFn(key, obj1, obj2),
+      newVal: newValFn(key, obj1, obj2),
       type,
-      children: null,
+      children: process(obj1[key], obj2[key], buildAST),
     }];
   }, []);
   return newArr;
